fef82fd331daa7cce069ea01fa4bad1da6432134
kevin <kevinfng12@gmail.com>
2023-05-12 17:17:50 -0400

    fixed merge errors

---
+++ src/config.py
@@ -0,0 +1,15 @@
+#TODO: Change config.py to a config.cfg file
+
+# dimensions of the game
+GAME_WIDTH = 640
+GAME_HEIGHT = 640
+
+# dimensions of each tile
+TILE_SIZE = GAME_WIDTH // 8
+
+# window border
+WINDOW_BORDER = GAME_WIDTH // 100
+
+# dimensions of the game window (includes borders)
+WINDOW_WIDTH = GAME_WIDTH + (WINDOW_BORDER * 2)
+WINDOW_HEIGHT = GAME_HEIGHT + (WINDOW_BORDER * 2)
