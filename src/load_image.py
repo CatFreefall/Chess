485fcdc5f9626a7a3c0a404e20d09fbac8025f9e
kevin <kevinfng12@gmail.com>
2023-05-12 15:50:17 -0400

    separated assets from config

---
+++ src/load_image.py
@@ -0,0 +1,7 @@
+import pygame as p
+
+from config import TILE_SIZE
+
+# This function loads an image from a file and scales it to the calculated tile size
+def load_image(location):
+  return p.transform.scale(p.image.load(location), (TILE_SIZE - 1, TILE_SIZE - 1))
