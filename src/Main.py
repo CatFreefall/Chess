cf7b9b761e2439b87e724867b6b82666121f149c
kevin <kevinfng12@gmail.com>
2023-05-12 12:17:01 -0400

    first commit

---
+++ src/Main.py
@@ -0,0 +1,12 @@
+import pygame as p
+from src.Config import *
+
+p.init()
+
+gameWindow = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+while True:
+  for event in p.event.get():
+    if event.type == p.QUIT:
+      p.quit()
+      quit()
